bf91e31df6d389280bd0de079b23450d69bd0072
tranvannghia021 <tranvannghia021@gmail.com>
2022-10-09 15:30:13 +0700

    (be): new branch silder and func custom slider

---
+++ be_shop_balo/app/Models/Slider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Slider extends Model
+{
+    use HasFactory, SoftDeletes;
+    protected $table = 'sliders';
+    protected $fillable = [
+        'name',
+        'description',
+        'image-slider',
+        'status',
+        'url'
+    ];
+}
