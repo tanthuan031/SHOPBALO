d5241874d9faef3683652d9da2924c6a07943293
tanthuan031 <tanthuan031@gmail.com>
2022-10-09 15:23:56 +0700

    (be): Update Folder

---
+++ be_shop_balo/app/Repositories/Admin/OrderRepository.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Repositories\Admin;
+
+use App\Http\Resources\Admin\Order\OrderDetailResource;
+use App\Http\Resources\Admin\Order\OrderResource;
+use App\Models\Order;
+use App\Models\OrderDetail;
+use App\Repositories\BaseRepository;
+
+
+class OrderRepository extends BaseRepository
+{
+    protected $order;
+    protected int $paginate = 10;
+    public function __construct(Order $order)
+    {
+        $this->order = $order;
+        parent::__construct($order);
+    }
+    public function getAllOrder($request)
+    {
+        $data = Order::query()
+            ->with('customers')
+            ->with('staff')
+            ->with('discounts')
+            // ->sort($request)
+            // ->filter($request)
+            ->search($request)
+            ->paginate($this->paginate);
+        return OrderResource::collection($data)->response()->getData();
+    }
+
+    public function getOrderDetailById($id)
+    {
+        $data = OrderDetail::query()
+            ->where('order_id', $id)
+            ->with('products')
+            ->get();
+        return
+            OrderDetailResource::collection($data)->response()->getData();
+    }
+    public function getOrderById($id)
+    {
+        $data = Order::query()->find($id);
+        return
+            OrderResource::make($data);
+    }
+    public function updateOrder($request, $id)
+    {
+        $order = Order::query()->where('id', '=', $id)->first();
+        if ($order) {
+            $order->update($request->all());
+            return $order;
+        } else {
+            return false;
+        }
+    }
+}
