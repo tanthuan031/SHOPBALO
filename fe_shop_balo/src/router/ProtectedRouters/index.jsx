c65128394573c8e604af9e98a3d32d0e8c8e5173
tanthuan031 <tanthuan031@gmail.com>
2022-10-22 17:04:00 +0700

    Merge login

--- fe_shop_balo/src/router/ProtectedRouters/index.jsx
+++ fe_shop_balo/src/router/ProtectedRouters/index.jsx
@@ -1,11 +1,9 @@
 // Check login authentication
 
 import * as React from 'react';
-import { useSelector } from 'react-redux';
 import { Navigate, Outlet } from 'react-router-dom';
 import { checkLogin } from '../../adapter/auth';
 // import { checkLogin } from '../../api/Auth';
-import { isLoginSelector } from '../../redux/selectors/auth/auth.reducer';
 
 export function ProtectedRoutes(props) {
   const isAuthenticate = checkLogin();
