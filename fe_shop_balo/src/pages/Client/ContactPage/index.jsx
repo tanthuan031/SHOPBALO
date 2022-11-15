be7001418ff8a3a35fd9ef558df718729bd36af6
tranvannghia021 <tranvannghia021@gmail.com>
2022-11-15 09:25:17 +0700

    (fe) page contact UI

--- fe_shop_balo/src/pages/Client/ContactPage/index.jsx
+++ fe_shop_balo/src/pages/Client/ContactPage/index.jsx
@@ -1,12 +1,62 @@
+
+import { FaEnvelope, FaMapMarked, FaPhone } from 'react-icons/fa';
 import './index.css';
 
 export function ContactPage() {
 
 
     return (
-       <div className="banner">
+        <>
+            <div className="wrapper">
+                <div className="banner">
+                    <p >contact</p>
+                </div>
+                <div className="content">
+                    <div className="content_right">
+                        <p>Send Us A Message</p>
+                        <form className='form_help'>
+                            <div className="group_input">
+                                <label htmlFor="id_email">Your Email Address</label>
+                                <input id="id_email" className='input_email' type="email" placeholder='Your Email Address' />
+                            </div>
+                            <div className="group_input">
+                                <label htmlFor="input_help">How Can We help ?</label>
+                                <textarea name="" id="input_help" placeholder='How Can We help ?' className='text_area_help'></textarea>
+                            </div>
+                        </form>
+                        <div className="wrapper_btn">
+                            <button className='btn_help'>submit</button>
+                        </div>
+                    </div>
+                    <div className="content_left">
+                        <div className="wrapper_help">
+                            <div className="group_title">
 
-       </div>
+                                <p> <FaMapMarked />
+                                    <span> Address</span></p>
+                                <p>Coza Store Center 8th floor, 379 Hudson St, New York, NY 10018 US</p>
+                            </div>
+                            <div className="group_title">
+
+                                <p> <FaPhone />
+                                    <span>
+                                        Lets Talk</span></p>
+                                <p className='text_color_cutom'>
+
+                                    (+1) 96 716 6879</p>
+                            </div>
+                            <div className="group_title">
+
+                                <p> <FaEnvelope />
+                                    <span>
+                                        Sale Support</span></p>
+                                <p className='text_color_cutom'>contact@example.com</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div >
+        </>
     );
 
 }
