f052fcadce611e874a885f7f67b7326678b4d10d
tanthuan031 <tanthuan031@gmail.com>
2022-11-22 16:17:49 +0700

    Fix error project complete .

--- fe_shop_balo/src/router/AdminRouter/index.jsx
+++ fe_shop_balo/src/router/AdminRouter/index.jsx
@@ -42,7 +42,6 @@ export default function AdminRouter() {
     });
   }, [dispatch]);
   const user = useSelector(getUserSelector);
-  console.log('d', user === null);
   return (
     <Routes>
       <Route path="/admin/login" element={<LoginPage />} />
