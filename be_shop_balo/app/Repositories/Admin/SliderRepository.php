79a19fe2317bf8cca522c3101e75e7fa3d4a53c3
minhtrung0110 <nguyenducminhtrung0044@gmail.com>
2022-12-10 16:27:14 +0700

    (be) :fix BE

--- be_shop_balo/app/Repositories/Admin/SliderRepository.php
+++ be_shop_balo/app/Repositories/Admin/SliderRepository.php
@@ -5,7 +5,7 @@ namespace App\Repositories\Admin;
 use App\Models\Slider;
 use App\Repositories\BaseRepository;
 
-class liderRepository extends BaseRepository
+class SliderRepository extends BaseRepository
 {
 
     public function __construct(Slider $slider)
